890dbfcce6d465853179df90c599c4f0ce10dfbb
MaartenWilloqué <r0237151@student.thomasmore.be>
2023-10-06 21:03:03 +0200

    init

---
+++ src/main/java/fact/it/inventoryservice/model/StockItem.java
@@ -0,0 +1,21 @@
+package fact.it.inventoryservice.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "stockitem")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class StockItem {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String skuCode;
+    private Integer quantity;
+}
